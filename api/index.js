283dfce86969c2135d8c8cc4550e9e819f30b5dd
kdn0325 <sexy-ehdsud@nate.com>
2022-02-11 18:32:07 +0900

    api-update,crypto-js-update

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./route/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+
+}).then(()=>console.log("DB Connection")).catch((err)=>console.log(err));
+
+app.use(express.json());
+app.use("/api/auth",authRoute);
+
+app.listen(8800,()=>{
+    console.log("Backend server is Running");
+});
